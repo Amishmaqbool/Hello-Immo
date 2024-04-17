6a448c029ccf5aa555d6a49ff900b06134dfedb5
Amishmaqbool <amishmaqbool@gmail.com>
2024-04-17 20:40:30 +0500

    Newsletter section added

--- src/components/SignupNewsletter.vue
+++ src/components/SignupNewsletter.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="bg-[#034465]">
-        <div class="p-6">
-            <h1 class="text-white text-[44px] font-bold">Wil jij graag op de hoogte blijven van ons laatste nieuws?</h1>
+  <div class="container mx-auto xl:w-[1360px] pt-20">
+    <div class="bg-[#034465] rounded-[20px]">
+      <div class="py-[80px] px-[220px] text-center">
+        <h1 class="text-white text-[36px] font-semibold">
+          Wil jij graag op de hoogte blijven van ons laatste nieuws?
+        </h1>
+        <p class="text-[20px] text-white pt-[36px]">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua.
+        </p>
+        <div class="pt-[40px] flex items-center justify-center gap-[18px]">
+          <input
+            class="rounded-full px-4 py-2 w-[380px]"
+            placeholder="E-mailadres"
+          />
+          <button class="text-[white] text-[20px] bg-[#D10909] px-4 py-2 rounded-full">
+            Inschrijven
+          </button>
         </div>
+      </div>
     </div>
+  </div>
 </template>
