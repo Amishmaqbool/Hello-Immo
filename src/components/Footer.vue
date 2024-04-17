187c1ff444bdb8401b2e7fd2ed89744886dc2295
Amishmaqbool <amishmaqbool@gmail.com>
2024-04-17 21:57:46 +0500

    Footer changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="mt-[160px] bg-[#F4F4F4] rounded-t-[86px]">
+    <div class="mt-[160px] bg-[#F4F4F4] rounded-t-[60px]">
         <div class="container mx-auto">
             <div class="flex justify-between">
-                <img src="/public/Logo.svg" alt="Logo">
+                <img class="mb-6" src="/public/Logo.svg" alt="Logo">
                 <div class="grid gap-[70px] grid-cols-3 pt-[105px]">
-                    <div class="text-[#023047]">
-                        <h6 class="font-medium text-[24px]">Menu</h6>
-                        <p class="font-normal text-[20px] pt-[21px] cursor-pointer">Nieuws</p>
-                        <p class="font-normal text-[20px] cursor-pointer">Over ons</p>
-                        <p class="font-normal text-[20px] cursor-pointer">Contact</p>
+                    <div class="text-[#023047] leading-tight">
+                        <h6 class="font-medium text-[24px] ">Menu</h6>
+                        <p class="font-medium text-[20px] pt-[15px] cursor-pointer">Nieuws</p>
+                        <p class="font-medium text-[20px] cursor-pointer">Over ons</p>
+                        <p class="font-medium text-[20px] cursor-pointer">Contact</p>
                     </div>
 
-                    <div class="text-[#023047]">
+                    <div class="text-[#023047] leading-tight">
                         <h6 class="font-medium text-[24px]">Panden te koop</h6>
-                        <p class="font-normal text-[20px] pt-[21px] cursor-pointer">Huis</p>
-                        <p class="font-normal text-[20px] cursor-pointer">Appartement</p>
-                        <p class="font-normal text-[20px] cursor-pointer">Nieuwbouwproject</p>
-                        <p class="font-normal text-[20px] cursor-pointer">Kantoor</p>
+                        <p class="font-medium text-[20px] pt-[15px] cursor-pointer">Huis</p>
+                        <p class="font-medium text-[20px] cursor-pointer">Appartement</p>
+                        <p class="font-medium text-[20px] cursor-pointer">Nieuwbouwproject</p>
+                        <p class="font-medium text-[20px] cursor-pointer">Kantoor</p>
                     </div>
 
                     <div class="text-[#023047]">
@@ -25,12 +25,12 @@
                 </div>
             </div>
 
-            <div class="pt-[163px] pb-[30px] flex justify-between items-center">
+            <div class="pt-[150px] pb-[30px] flex justify-between items-center">
                 <div class="flex gap-[37px]">
-                    <p class="font-normal text-[20px] text-[#023047] cursor-pointer">Privacy policy</p>
-                    <p class="font-normal text-[20px] text-[#023047] cursor-pointer">Cookiestatement</p>
+                    <p class="font-normal text-[20px] text-[#023047] cursor-pointer opacity-[0.50]">Privacy policy</p>
+                    <p class="font-normal text-[20px] text-[#023047] cursor-pointer opacity-[0.50]">Cookiestatement</p>
                 </div>
-                <p class="font-normal text-[20px] text-[#023047]">Madeby</p>
+                <p class="font-normal text-[20px] text-[#023047] opacity-[0.50]">Madeby</p>
             </div>
         </div>
     </div>
