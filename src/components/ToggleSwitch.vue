612dd4689eb9411ed0f8a7008995da158783c97c
Amishmaqbool <amishmaqbool@gmail.com>
2024-04-17 22:52:50 +0500

    Footer changes

--- src/components/ToggleSwitch.vue
+++ src/components/ToggleSwitch.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="inline-flex rounded-full shadow-sm">
+    <div class="inline-flex rounded-full bg-white py-1 pl-1 shadow-sm">
       <button
         :class="isForSale ? 'bg-red-600 text-white' : 'text-gray-600 bg-white'"
-        class="px-4 py-2 rounded-l-full focus:outline-none transition-colors"
+        class="px-4 py-2 rounded-full focus:outline-none transition-colors"
         @click="toggle('forSale')"
       >
         TE KOOP
       </button>
       <button
         :class="!isForSale ? 'bg-red-600 text-white' : 'text-gray-600 bg-white'"
-        class="px-4 py-2 rounded-r-full focus:outline-none transition-colors"
+        class="px-4 py-2 rounded-full focus:outline-none transition-colors"
         @click="toggle('forRent')"
       >
         TE HUUR
