f09e2703a68172542db4db4e5aaeba43b1f061ff
Amishmaqbool <amishmaqbool@gmail.com>
2024-04-18 02:15:33 +0500

    Footer Responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,54 +4,48 @@ import ToogleSwitch from "../components/ToggleSwitch.vue";
 <template>
   <div>
     <div class="mt-[160px] bg-[#F4F4F4] rounded-t-[60px]">
-      <div class="container mx-auto">
-        <div class="flex justify-between">
-        <div class="pt-[90px]">
-          <img class="mb-8 cursor-pointer" src="/Logo.svg" alt="Logo" />
+      <div class="px-5 md:px-6 lg:px-0 lg:container mx-auto">
+        <div class="flex flex-col lg:flex-row justify-between">
+        <div class="pt-[60px] lg:pt-[0px]">
+          <img class="mb-8 cursor-pointer h-12 lg:h-full w-64 lg:w-full" src="/Logo.svg" alt="Logo" />
           <ToogleSwitch />
         </div>
-          <div class="grid gap-[70px] grid-cols-3 pt-[105px]">
+          <div class="flex flex-col sm:flex-row xl:grid gap-8 sm:gap-8 md:gap-7 lg:gap-[50px] xl:gap-[70px] grid-cols-3 pt-[35px] md:pt-[75px] lg:pt-[105px]">
             <div class="text-[#023047] leading-tight">
-              <h6 class="font-medium text-[24px]">Menu</h6>
-              <p class="font-medium text-[20px] pt-[15px] cursor-pointer">
-                Nieuws
-              </p>
-              <p class="font-medium text-[20px] cursor-pointer">Over ons</p>
-              <p class="font-medium text-[20px] cursor-pointer">Contact</p>
+              <h6 class="font-medium text-[20px] lg:text-[24px]">Menu</h6>
+              <p class="font-medium text-4 lg:text-[20px] pt-[15px] cursor-pointer">Nieuws</p>
+              <p class="font-medium text-4 lg:text-[20px] cursor-pointer">Over ons</p>
+              <p class="font-medium text-4 lg:text-[20px] cursor-pointer">Contact</p>
             </div>
 
             <div class="text-[#023047] leading-tight">
-              <h6 class="font-medium text-[24px]">Panden te koop</h6>
-              <p class="font-medium text-[20px] pt-[15px] cursor-pointer">
-                Huis
-              </p>
-              <p class="font-medium text-[20px] cursor-pointer">Appartement</p>
-              <p class="font-medium text-[20px] cursor-pointer">
-                Nieuwbouwproject
-              </p>
-              <p class="font-medium text-[20px] cursor-pointer">Kantoor</p>
+              <h6 class="font-medium text-[20px] lg:text-[24px]">Panden te koop</h6>
+              <p class="font-medium text-4 lg:text-[20px] pt-[15px] cursor-pointer">Huis</p>
+              <p class="font-medium text-4 lg:text-[20px] cursor-pointer">Appartement</p>
+              <p class="font-medium text-4 lg:text-[20px] cursor-pointer">Nieuwbouwproject</p>
+              <p class="font-medium text-4 lg:text-[20px] cursor-pointer">Kantoor</p>
             </div>
 
             <div class="text-[#023047]">
-              <h6 class="pr-[40px] font-medium text-[24px]">Hypotheeklening</h6>
+              <h6 class="pr-0 lg:pr-[40px] font-medium text-[20px] lg:text-[24px]">Hypotheeklening</h6>
             </div>
           </div>
         </div>
 
-        <div class="pt-[150px] pb-[30px] flex justify-between items-center">
+        <div class="pt-[100px] lg:pt-[150px] pb-[30px] flex justify-between flex-wrap items-center">
           <div class="flex gap-[37px]">
             <p
-              class="font-normal text-[20px] text-[#023047] cursor-pointer opacity-[0.50]"
+              class="font-normal text-4 lg:text-[20px] text-[#023047] cursor-pointer opacity-[0.50]"
             >
               Privacy policy
             </p>
             <p
-              class="font-normal text-[20px] text-[#023047] cursor-pointer opacity-[0.50]"
+              class="font-normal text-4 lg:text-[20px] text-[#023047] cursor-pointer opacity-[0.50]"
             >
               Cookiestatement
             </p>
           </div>
-          <p class="font-normal text-[20px] text-[#023047] opacity-[0.50]">
+          <p class="font-normal text-4 lg:text-[20px] text-[#023047] opacity-[0.50]">
             Madeby
           </p>
         </div>
--- src/components/ToggleSwitch.vue
+++ src/components/ToggleSwitch.vue
@@ -2,14 +2,14 @@
     <div class="inline-flex rounded-full bg-white py-1 pl-1 pr-1 shadow-sm">
       <button
         :class="isForSale ? 'bg-red-600 text-white' : 'text-gray-600 bg-white'"
-        class="px-4 py-2 rounded-full focus:outline-none transition-colors"
+        class="px-4 py-[2px] lg:py-2 rounded-full focus:outline-none transition-colors"
         @click="toggle('forSale')"
       >
         TE KOOP
       </button>
       <button
         :class="!isForSale ? 'bg-red-600 text-white' : 'text-gray-600 bg-white'"
-        class="px-4 py-2 rounded-full focus:outline-none transition-colors"
+        class="px-4 py-[2px] lg:py-2 rounded-full focus:outline-none transition-colors"
         @click="toggle('forRent')"
       >
         TE HUUR
