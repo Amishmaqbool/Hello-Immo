bc2c33e8bf1dc37945c16de2d585b4f04ae8828d
Amishmaqbool <amishmaqbool@gmail.com>
2024-04-21 02:12:39 +0500

    Site configs updated

--- src/components/MortageLoan.vue
+++ src/components/MortageLoan.vue
@@ -2,7 +2,9 @@
   <div class="bg-[#034465] lg:rounded-br-[120px]">
     <div
       class="py-[60px] 2xl:max-w-[1640px] container mx-auto sm:px-0 px-5"
-      :class="{ 'lg:py-0 2xl:max-w-full min-[2000px]:max-w-[1640px] pr-5': isSearchPage }"
+      :class="{
+        'lg:py-0 2xl:max-w-full min-[2000px]:max-w-[1640px] pr-5': isSearchPage,
+      }"
     >
       <div class="lg:flex items-center gap-32 2xl:gap-40">
         <div
@@ -21,7 +23,7 @@
             :class="{ hidden: isSearchPage }"
             width="800"
             height="532"
-          >
+          />
           <img
             src="../assets/MortageFamilyS.png"
             alt="MortageFamily"
@@ -29,28 +31,24 @@
             :class="{ hidden: !isSearchPage }"
             width="950"
             height="532"
-          >
+          />
         </div>
         <div
           class="lg:w-[520px] relative lg:pt-0 pt-5"
           :class="{ 'py-[60px] lg:pt-[60px] pt-[65px] pr-5': isSearchPage }"
         >
           <h1
-            class="text-[44px] text-white font-[Cabin] font-extrabold leading-[50px] relative"
+            class="text-[44px] text-white font-[Cabin] font-extrabold leading-[50px] relative z-50"
           >
             Wij helpen je met jouw
-            <span class="z-50">
-              hypothecaire lening
-              <div class="2xl:block hidden">
-                <img
-                  src="/MortageHeadingLine.png"
-                  alt="underline"
-                  class="w-[394px] mt-[-30px] ml-[-10px] z-[-50]"
-                  style="transform: translateY(10%)"
-                >
-              </div>
-            </span>
+            <span class="z-50"> hypothecaire lening </span>
           </h1>
+          <img
+            src="/MortageHeadingLine.png"
+            alt="underline"
+            class="w-[394px] mt-[-25px] ml-[-10px] z-0"
+            style="transform: translateY(10%)"
+          />
           <p class="text-white text-[20px] pt-[30px]">
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
@@ -61,10 +59,7 @@
             class="bg-white text-[#023047] font-medium sm:text-[22px] rounded-[25px] px-8 py-2 mt-[60px] flex gap-2 items-center"
           >
             Vraag uw hypotheek lening aan
-            <img
-              src="/ArrowRight.svg"
-              alt="MortageFamily"
-            >
+            <img src="/ArrowRight.svg" alt="MortageFamily" />
           </button>
         </div>
       </div>
