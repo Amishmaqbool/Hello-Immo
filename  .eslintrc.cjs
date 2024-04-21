c5bf41cd1b3705bbe9b70f3793e3254250bb7614
Amishmaqbool <amishmaqbool@gmail.com>
2024-04-21 21:58:31 +0500

    Eslint and prettier updated

---
+++  .eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+    root: true,
+    'extends': [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        "vue/script-setup-uses-vars": "error",
+        "vue/multi-word-component-names": "off",
+        "no-mixed-spaces-and-tabs": 0,
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+}
